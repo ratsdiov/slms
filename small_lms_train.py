caa1277103ff8e16d795055845356a71abb61799
Keith Davidson <kbdsp@hotmail.com>
2024-02-01 13:37:36 -0500

    2024-01-31-3-epoch-7.607M

--- small_lms_train.py
+++ small_lms_train.py
@@ -8,10 +8,11 @@ from tokenizers import Tokenizer
 
 from tqdm.auto import tqdm
 from nano_gpt_model import NanoGPT
+
 # -----------------------------
 
 # setup cuda
-torch.cuda.memory._record_memory_history()
+torch.cuda.memory._record_memory_history(enabled=True)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 torch.set_default_device("cpu")
 print(f"using {device}")
@@ -25,38 +26,41 @@ tokenizer = Tokenizer.from_file(tokenizer_file)
 
 # hyperparameters
 hyperparameters = {
-  "n_epochs": 3,
-  "vocab_size": tokenizer.get_vocab_size(),
-  "batch_size": 8,
-  "block_size": 1080,
-  "learning_rate": 1.5e-2,
-  "n_embed": 256,
-  "n_heads": 2,
-  "n_layers": 4,
-  "dropout": 0.1,
+    "n_epochs": 5,
+    "vocab_size": tokenizer.get_vocab_size(),
+    "batch_size": 4,
+    "block_size": 1080,
+    "learning_rate": 5e-4,
+    "n_embed": 256,
+    "n_heads": 12,
+    "n_layers": 8,
+    "dropout": 0.1,
 }
 
-n_epochs = hyperparameters['n_epochs']
-vocab_size = hyperparameters['vocab_size']
-batch_size = hyperparameters['batch_size']
-block_size = hyperparameters['block_size']
-learning_rate = hyperparameters['learning_rate']
-n_embed = hyperparameters['n_embed']
-n_heads = hyperparameters['n_heads']
-n_layers = hyperparameters['n_layers']
-dropout = hyperparameters['dropout']
+n_epochs = hyperparameters["n_epochs"]
+vocab_size = hyperparameters["vocab_size"]
+batch_size = hyperparameters["batch_size"]
+block_size = hyperparameters["block_size"]
+learning_rate = hyperparameters["learning_rate"]
+n_embed = hyperparameters["n_embed"]
+n_heads = hyperparameters["n_heads"]
+n_layers = hyperparameters["n_layers"]
+dropout = hyperparameters["dropout"]
 # -----------------------------
 
 # tokenize dataset
 tokenizer.enable_padding(pad_id=2, pad_token="<|im_end|>", length=block_size)
 tokenizer.enable_truncation(max_length=block_size)
-tokenized_data = dataset.map(lambda x: { "input_ids": [elem.ids for elem in tokenizer.encode_batch(x['text'])] }, batched=True)
+tokenized_data = dataset.map(
+    lambda x: {"input_ids": [elem.ids for elem in tokenizer.encode_batch(x["text"])]},
+    batched=True,
+)
 tokenized_data = tokenized_data.with_format("torch")
 
-train_ids = tokenized_data['train'].remove_columns(['text'])
+train_ids = tokenized_data["train"].remove_columns(["text"])
 train_ids = train_ids.shuffle().select(range(30000))
 
-val_ids = tokenized_data['validation'].remove_columns(['text'])
+val_ids = tokenized_data["validation"].remove_columns(["text"])
 val_ids = val_ids.shuffle().select(range(3000))
 # -----------------------------
 
@@ -66,10 +70,12 @@ train_dataloader = DataLoader(train_ids, batch_size=batch_size, shuffle=True)
 val_dataloader = DataLoader(val_ids, batch_size=batch_size, shuffle=True)
 
 optimizer = AdamW(model.parameters(), lr=learning_rate)
-num_params = sum(p.numel() for p in model.parameters())/1e6
+num_params = sum(p.numel() for p in model.parameters()) / 1e6
 
 num_training_steps = n_epochs * len(train_dataloader)
-scheduler = lr_scheduler.OneCycleLR(optimizer=optimizer, max_lr=learning_rate, total_steps=num_training_steps)
+scheduler = lr_scheduler.OneCycleLR(
+    optimizer=optimizer, max_lr=learning_rate, total_steps=num_training_steps
+)
 # scheduler = lr_scheduler.StepLR(optimizer, step_size=1000, gamma=0.8)
 print(f"{num_params:.3f}M parameters")
 # -----------------------------
@@ -92,37 +98,43 @@ lri = []
 progress_bar = tqdm(range(num_training_steps))
 
 for epoch in range(n_epochs):
-    model.train() # switch model to training mode
+    model.train()  # switch model to training mode
     if saved_epoch != None and epoch <= saved_epoch:
         continue
-      
+
     for batch in train_dataloader:
-        batch = batch['input_ids'].to(device)
-        targets = torch.concat((batch[:, 1:], 2 * torch.ones([batch.shape[0], 1]).to(device)), dim=1).long()
+        batch = batch["input_ids"].to(device)
+        targets = torch.concat(
+            (batch[:, 1:], 2 * torch.ones([batch.shape[0], 1]).to(device)), dim=1
+        ).long()
         logits, loss = model(batch, targets)
-        
+
         optimizer.zero_grad()
         loss.backward()
 
         optimizer.step()
         scheduler.step()
         progress_bar.update(1)
-        
-        if (progress_bar.n % 500 == 0):
-            print(f"erratic train loss: {loss.item()} lr: {optimizer.param_groups[0]['lr']}")
+
+        if progress_bar.n % 500 == 0:
+            print(
+                f"erratic train loss: {loss.item()} lr: {optimizer.param_groups[0]['lr']}"
+            )
         lossi.append(loss.log10().item())
-        lri.append(optimizer.param_groups[0]['lr'])
-            
+        lri.append(optimizer.param_groups[0]["lr"])
+
     with torch.no_grad():
         # evaluate validation loss
-        model.eval() # switch model to evaluation mode
-        losses =  torch.zeros(len(val_dataloader), device=device)
+        model.eval()  # switch model to evaluation mode
+        losses = torch.zeros(len(val_dataloader), device=device)
         k = 0
         for batch in val_dataloader:
-            batch = batch['input_ids'].to(device)
-            targets = torch.concat((batch[:, 1:], 2 * torch.ones([batch.shape[0], 1]).to(device)), dim=1).long()
+            batch = batch["input_ids"].to(device)
+            targets = torch.concat(
+                (batch[:, 1:], 2 * torch.ones([batch.shape[0], 1]).to(device)), dim=1
+            ).long()
             logits, loss = model(batch, targets)
-                
+
             losses[k] = loss.item()
             predictions = torch.argmax(logits, dim=-1)
             k += 1
@@ -130,26 +142,28 @@ for epoch in range(n_epochs):
         avg_val_loss = losses.mean()
         print(f"val loss: {avg_val_loss}")
         # -----------------------------
-        
+
         # evaluate training loss
-        losses =  torch.zeros(len(val_dataloader), device=device)
+        losses = torch.zeros(len(val_dataloader), device=device)
         k = 0
         for batch in train_dataloader:
-            batch = batch['input_ids'].to(device)
-            targets = torch.concat((batch[:, 1:], 2 * torch.ones([batch.shape[0], 1]).to(device)), dim=1).long()
+            batch = batch["input_ids"].to(device)
+            targets = torch.concat(
+                (batch[:, 1:], 2 * torch.ones([batch.shape[0], 1]).to(device)), dim=1
+            ).long()
             logits, loss = model(batch, targets)
-                
+
             losses[k] = loss.item()
             predictions = torch.argmax(logits, dim=-1)
             k += 1
-            
-            if(k == len(val_dataloader)):
+
+            if k == len(val_dataloader):
                 break
-        
+
         avg_train_loss = losses.mean()
         print(f"train loss: {avg_train_loss}")
         # -----------------------------
-        
+
         # save checkpoint
         checkpoint = {
             "epoch": epoch,
@@ -160,9 +174,12 @@ for epoch in range(n_epochs):
             "val_loss": avg_val_loss,
             "train_loss": avg_train_loss.item(),
         }
-        torch.save(checkpoint, f"checkpoints/3-epoch-{num_params:.3f}M-checkpoint-{epoch}.pt")
+        torch.save(
+            checkpoint, f"checkpoints/3-epoch-{num_params:.3f}M-checkpoint-{epoch}.pt"
+        )
         # -----------------------------
 # -----------------------------
 
 from matplotlib import pyplot as plt
+
 plt.plot(torch.tensor(lossi).view(-1, 25).mean(1))
